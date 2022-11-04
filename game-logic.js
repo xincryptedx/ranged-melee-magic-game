dd13754a3744dd13984580040fe4fd416df312a3
Joshua Barnhart <joshua.r.barnhart@gmail.com>
2022-11-04 12:56:31 -0400

    Add pseudocode to game-logic.js

---
+++ game-logic.js
@@ -0,0 +1,19 @@
+//Melee Ranged Magic is a rock paper scissors clone based on the combat triange in the popular MMORPG Runescape.
+//Melee beats Ranged. Ranged beats Magic. Magic beats Melee.
+
+//Program Flow
+//Player initiates a new game. Games consist of five rounds and are called early if either the player or
+//computer reach three points.
+
+//After a new game has begun, the computer randomly decides its choice.
+
+//The player is prompted to make their choice.
+
+//The choices are compared against each other and the winner is decided.
+
+//The scores are updated.
+
+//If either player or computer has 3 points, the game is called and winner declared. The player is prompted
+//to begin a new game if they wish.
+
+//If neither player has 3 points yet, the next round starts with the computer randomly deciding its choice.
