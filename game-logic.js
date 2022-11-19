4f57b6aad20e56089db2cf57cd6197ef13731cc1
Joshua Barnhart <joshua.r.barnhart@gmail.com>
2022-11-19 16:58:57 -0500

    Remove redundant comments and empty lines

--- game-logic.js
+++ game-logic.js
@@ -1,8 +1,6 @@
 //Melee Ranged Magic is a rock paper scissors clone based on the combat triange in the popular MMORPG Runescape.
 //Melee beats Ranged. Ranged beats Magic. Magic beats Melee.
 
-//Program Flow
-
 //Variables
 let playerSelection = "Melee"; //Set to Melee for testing purposes. Change to "" later.
 let computerSelection = "";
@@ -13,20 +11,18 @@ let computerScore = 0;
 
 const winningScore = 5;
 
-//Object that defines selection outcomes
+//Object that defines attack selection outcomes
 const attacks = {
     ranged : {weak: "melee", strong: "magic"},
     melee : {weak: "magic", strong: "ranged"},
     magic : {weak: "ranged", strong: "melee"}
 }
 
-//Player initiates a new game. Game is over when player or computer has 5 points.
 function game(){
 
     initializeGame();
 
     while (playerScore < winningScore && computerScore < winningScore){
-
         computerSelection = getComputerSelection();
         playerSelection = getPlayerSelection();
 
@@ -80,29 +76,24 @@ function gameTie(){
 
 //Gets player function while giving current round and score
 function getPlayerSelection(){
-    //Display a message with current round and scores, and ask for player input
     return prompt("Round " + round + ". Score: Player " + playerScore + ", Computer " + computerScore + ". Choose attack style.");
 }
 
-//Checks to see if player selection is a valid choice
-function verifyPlayerSelection(selection){
-    
-    //Make sure selection is valid. If not ask for new selection.
-    selection = selection.toLowerCase();            //lowercase for comparison
 
-    if (!attacks.hasOwnProperty(selection))       //if the input is not in the attacks object display alert and return false
+function verifyPlayerSelection(selection){
+    selection = selection.toLowerCase();            
+
+    if (!attacks.hasOwnProperty(selection))       
     {
         alert("That is not a valid choice. Please choose Melee, Magic, or Ranged.");
         return false;
     }
     else
     {
-        return true;                         //if input is valid, return true
+        return true;                         
     }
-
 }
 
-//Sets variables to starting values for new game
 function initializeGame(){
     round = 1;          
     playerScore = 0;    
@@ -111,22 +102,18 @@ function initializeGame(){
 
 //One round of comparing player and computer selections
 function playRound(playerSelection, computerSelection){
-
-    //Ensure selections are in lowercase for accurrate comparisons
     computerSelection = computerSelection.toLowerCase();
     playerSelection = playerSelection.toLowerCase();
 
     //Player wins
     if (attacks[playerSelection].strong === computerSelection)
     {
-        //Update playerScore and return message
         playerScore ++;
         return capitalize(playerSelection) + " is strong to " + capitalize(computerSelection) + ". You win!"
     }
     //Player loses
     if (attacks[playerSelection].weak === computerSelection)
     {
-        //Update computerScore and return message
         computerScore++;
         return capitalize(playerSelection) + " is weak to " + capitalize(computerSelection) + ". You lose!"
     }
@@ -137,11 +124,10 @@ function playRound(playerSelection, computerSelection){
     }
 }
 
-//After a new game has begun, the computer randomly decides its choice.
 function getComputerSelection (){
     let choice = getRandomInt(1,3);
 
-    switch(choice){                 //Return ranged, melee, or magic based on outcome
+    switch(choice){                 
         case 1:
             return "ranged";
         case 2:
@@ -152,22 +138,11 @@ function getComputerSelection (){
             return "Computer selection error";
     }
 }
-//Get a random int between 1 and 3
+
 function getRandomInt (min, max){
     return Math.floor(Math.random() * (max - min + 1) + min);
 }
-//This capitalizes the first letter in a string. Useful for formatting selections for outcome messages.
+
 function capitalize(text){
     return text.charAt(0).toUpperCase() + text.slice(1).toLowerCase();
 }
-
-//The player is prompted to make their choice.
-
-//The choices are compared against each other and the winner is decided.
-
-//The scores are updated.
-
-//If either player or computer has 3 points, the game is called and winner declared. The player is prompted
-//to begin a new game if they wish.
-
-//If neither player has 3 points yet, the next round starts with the computer randomly deciding its choice.
